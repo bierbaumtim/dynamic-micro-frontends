d3bb562e906d91b578c6d31006f4d16bddc99677
bierbaumtim <40519580+bierbaumtim@users.noreply.github.com>
2024-07-28 19:51:39 +0200

    regenerated todo list as plain svelte project

--- my-todo-list/vite.config.ts
+++ my-todo-list/vite.config.ts
@@ -1,6 +1,44 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import { resolve } from 'path'
 
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+//   build: {
+//     lib: {
+//       entry: resolve(__dirname, 'src/main.ts'),
+//       name: 'MyTodoListComponent',
+//       fileName: (format) => `my-todo-list.${format}.js`,
+//       formats: ['es'],
+//     },
+//     outDir: 'dist-js',
+//   }
+// })
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({}),
+      exclude: /\.component\.svelte$/,
+      emitCss: false,
+    }),
+    svelte({
+      preprocess: sveltePreprocess(),
+      include: /\.component\.svelte$/,
+      compilerOptions: {
+        customElement: true,
+      },
+      emitCss: false,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    target: 'modules',
+    lib: {
+      entry: 'src/main.ts',
+      name: '<<name>>',
+      fileName: 'components',
+    },
+  },
+})
