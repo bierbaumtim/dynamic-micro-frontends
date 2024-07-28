7bc872b54b5c453b674d450f21bd3ca8236ed7c4
bierbaumtim <40519580+bierbaumtim@users.noreply.github.com>
2024-07-28 19:55:09 +0200

    implement feature loading

---
+++ My-FrameApp/My-FrameApp/wwwroot/featureLoader.js
@@ -0,0 +1,10 @@
+﻿window.loadFeature = async function (url, custom_element_name) {
+  if (!window.customElements.get(custom_element_name)) {
+    var script = document.createElement("script");
+    script.id = `init_${custom_element_name}_script`;
+    script.type = "module";
+    script.src = url;
+
+    document.head.appendChild(script);
+  }
+};
